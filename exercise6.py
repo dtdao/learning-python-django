838dfd042083ca0ed5faab240e355b2b5bca8b91
kwality <kwalitycontrol@gmail.com>
2019-01-06 16:54:56 +0900

    exercise 6

---
+++ exercise6.py
@@ -0,0 +1,9 @@
+word = (input("Please enter a string >>>>>> ").lower())
+
+
+reverse = word.lower() [::-1]
+
+if word == reverse :
+	print("your word is Palindrome")
+else:
+	print("your word is not a Palindrome")
