739abda7925448ce631616abeec56ec3749946a8
kwality <kwalitycontrol@gmail.com>
2019-01-06 14:51:51 +0900

    exercise 3 changes

--- exercise3.py
+++ exercise3.py
@@ -1,11 +1,21 @@
 number_list = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 
+
+
 def print_out_less_than_five(array):
+
+	while True:
+		try: 
+			number = int(input("Please give me a number >>>>> "))
+			assert(number > 0)
+			break
+		except: 
+			print("You provided a string and non zero number, try again.")
 
 	new_array = []
 
 	for num in array:
-		if(num < 5):
+		if(num < number):
 			print(num)
 			new_array.append(num)
 
