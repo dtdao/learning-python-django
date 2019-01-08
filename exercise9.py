68270b0a2118d1d767567e95e4c352bdc8e29f43
kwality <kwalitycontrol@gmail.com>
2019-01-08 20:51:29 +0900

    exercise 9

---
+++ exercise9.py
@@ -0,0 +1,38 @@
+import random
+
+totalGuess = 0
+game_on = ""
+
+def guess(answer):
+	global game_on, totalGuess
+	while True:
+		try:
+			my_guess = int(input(">>>>> "))
+			totalGuess += 1
+		except:
+			totalGuess += 1
+			print("You entered a invalid value")
+			break
+
+		if int(my_guess) < answer :
+			print(answer)
+			print("**** Your guess is too low ****")
+			continue
+		elif int(my_guess) > answer :
+			print(answer)
+			print("**** Your guess is too high ****")
+			continue
+		else:
+			print("**** Your guess is correct! *****")
+			break
+	game_on = str(input("Do you want to exit? >>>> "))	
+
+while game_on != "exit":
+	random_number = random.randint(1, 9)
+	print("Please guess a number between 1 and 9")
+	guess(random_number)
+else:
+	print(f"You guessed a total of {totalGuess} times")
+	print("Game over")
+
+
