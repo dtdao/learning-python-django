d564eea3822c11ea554d297e6308f7aa603e869d
kwality <kwalitycontrl@gmail.com>
2019-01-11 15:42:10 +0900

    schol work update not finished

--- exercise13.py
+++ exercise13.py
@@ -2,21 +2,35 @@
 
 how_many_times = int(input("How many Fibonacci number to generate? >>>> "))
 count = 0
-holder = [1]
+holder = [1,2,3,4,5,6]
 
-def fibonacci_number_please(holder):
-	if len(holder) == 1:
-		return [1, 1]
-	return holder.append(holder.pop() + holder[len(holder) - 2])
+new_number = holder[len(holder) - 2]
+last_number = holder.pop()
 
-while True:
-	if count <= how_many_times:
-		count += 1
-		print(fibonacci_number_please(holder));
-	else:
-		break
-		print("finished")
+new = last_number + new_number
+print(last_number)
+print(new_number)
+
+print(new)
 
 
 
+print(holder)
 
+
+# def fibonacci_number_please(holder):
+# 	if len(holder) == 1:
+# 		return [1, 1]
+# 	if len(holer) >= 1:
+# 		print(holder)
+# 		fibonacci_number_please(holder.append(holder.pop() + holder[len(holder) - 2]))
+# 	else:
+# 		return holder
+#
+# while True:
+# 	if count <= how_many_times:
+# 		count += 1
+# 		print(fibonacci_number_please(holder));
+# 	else:
+# 		print("finished")
+# 		break
